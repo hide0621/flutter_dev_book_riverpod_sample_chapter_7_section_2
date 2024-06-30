ac6970f6edd43a3db6b95326d4a5e4a12e21862c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-07-01 08:57:13 +0900

    Riverpodが使えるようにパッケージを導入した

--- lib/riverpod/sample.dart
+++ lib/riverpod/sample.dart
@@ -1,21 +1,21 @@
 /// Riverpod
 /// Providerを活用した最小のサンプル
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-//
-// final greetProvider = Provider((ref) {
-//   return 'Hello, Flutter!!';
-// });
-//
-// class HomePage extends ConsumerWidget {
-//   const HomePage({super.key});
-//
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final greet = ref.watch(greetProvider);
-//     return Center(
-//       child: Text(greet),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final greetProvider = Provider((ref) {
+  return 'Hello, Flutter!!';
+});
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final greet = ref.watch(greetProvider);
+    return Center(
+      child: Text(greet),
+    );
+  }
+}
