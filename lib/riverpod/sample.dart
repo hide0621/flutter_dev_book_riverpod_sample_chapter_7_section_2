950582893901a5b8cc6a5763f656b404336fdf1e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-07-01 09:38:26 +0900

    Riverpodについてコメントを追記

--- lib/riverpod/sample.dart
+++ lib/riverpod/sample.dart
@@ -1,5 +1,10 @@
 /// Riverpod
 /// Providerを活用した最小のサンプル
+///
+/// Provider: 状態をキャッシュする
+/// RefやWidgetRef: これを介して状態を提供する
+/// ConsumerWidget: ウィジェットのサブクラス、buildメソッドの引数にWidgetRefが渡され、これを介して状態を取得してウィジェットを構築する
+/// Providerの持つ状態が変化すると、buildメソッドが再び呼び出される
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
